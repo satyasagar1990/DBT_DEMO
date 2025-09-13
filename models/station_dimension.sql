e4d6c8c1dac83d0a6a80a8dcb92d81acb540fb8d
satyasagar Mohapatra <mailing2satyasagar@gmail.com>
2025-09-13 10:33:53 +0000

    station_dimension commit

---
+++ models/station_dimension.sql
@@ -0,0 +1,12 @@
+with
+    station_cte as (
+        select distinct
+            start_statio_id as station_id,
+            start_station_name as station_name,
+            start_lat,
+            start_lng
+        from {{ source("DEMO", "BIKE") }}
+        where ride_id != 'ride_id'
+    )
+select *
+from station_cte
